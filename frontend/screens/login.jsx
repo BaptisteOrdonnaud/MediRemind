dcbbd2cb8869d9fe830d025e2b8912f5b3b14c14
Baptiste Ordonnaud <baptisteordonnaud@gmail.com>
2024-03-04 11:59:24 +0100

    hkdkbs

--- frontend/screens/login.jsx
+++ frontend/screens/login.jsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Image source={require('../assets/TemplateImage.png')}/>
-      <Text>MediRemind</Text>
-      
+      <Text>MediReminder</Text>
+    
       <StatusBar style="auto" />
     </View>
   );
