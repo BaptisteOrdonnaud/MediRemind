0189701f35b68a4d2431edfbc819f738591959b1
Your name <Your email>
2024-03-05 14:59:09 +0100

    menu home

--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
+
 import { useDispatch, useSelector } from 'react-redux';
 
-
-export default function HomeScreen(props) {
+export default function HomeScreen() {
 
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.value);
@@ -13,30 +12,29 @@ export default function HomeScreen(props) {
   const prenom = user.prenom
   const nom = user.nom
 
-
   return (
-   <SafeAreaView style={styles.container}>
-   <View style={styles.container}>
-<View style={styles.headerContainer}>
-<Image style={styles.image} source={require('../assets/TemplateImage.png')} />
-<Text style={styles.headerText}>Hello {prenom + ' ' + nom}</Text>
-</View>
-      <View style={styles.calendarContainer}>
-      <Calendar
-          current={new Date()} // Affiche la semaine actuelle
-          style={{ height: 100, width: '100%' }} // Hauteur pour une semaine et largeur totale
-          hideExtraDays={true} // Masque les jours des autres semaines
-          hideDayNames={false} // Affiche les noms des jours
-          firstDay={1} // Démarre la semaine avec Lundi
-          theme={{
-            arrowColor: '#7368BF', // Couleur des flèches de navigation
-            textSectionTitleColor: '#7368BF', // Couleur du titre de la section de texte
-            todayTextColor: '#7368BF', // Couleur du texte pour la date d'aujourd'hui
-          }}
-        />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Image style={styles.image} source={require('../assets/TemplateImage.png')} />
+          <Text style={styles.headerText}>Hello {prenom + ' ' + nom}</Text>
         </View>
-      <StatusBar style="auto" />
-    </View>
+        <View style={styles.todayTreatment}>
+          <Text>Traitement à prendre aujourd'hui!</Text>
+        </View>
+        <View style={styles.treatmentContainer}>
+
+<View style={styles.treatment}>
+  <Text>c'est à la place de ce text que les medicament arrive</Text>
+  <TouchableOpacity style={styles.buttonModif} activeOpacity={0.8}  > 
+        <Text style={styles.textButton}>Confirmer la prise</Text>
+      </TouchableOpacity>
+</View>
+
+
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </SafeAreaView>
   );
 }
@@ -47,30 +45,71 @@ const styles = StyleSheet.create({
     backgroundColor: '#E1DFFF',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    flexDirection: 'column',
   },
   headerContainer: {
-    backgroundColor:'#7368BF',
-    flexDirection:'row',
-    width:'100%',
-    height:90,
-    justifyContent:'center',
-    alignItems:'center',
-top:50,
+    backgroundColor: '#F0EFFF',
+    flexDirection: 'row',
+    width: '100%',
+    height: 90,
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 50,
   },
   headerText: {
-    fontSize:20,
-    fontWeight:'bold',
-    marginLeft:'auto',
-    marginRight:'auto',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   image: {
     width: 50,
-    height:50,
-    marginLeft:10,
-   },
-   calendarContainer: {
-    width: '100%',
-    alignItems: 'center', // Centre le calendrier horizontalement
-    marginTop: 20, // Espacement entre l'en-tête et le calendrier
-   },
+    height: 50,
+    marginLeft: 10,
+    borderRadius: 9999,
+  },
+  todayTreatment: {
+    backgroundColor: 'white',
+    width: 350,
+    height: 200,
+    marginTop: 200, // Adjust margin top as needed
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  treatmentContainer:{
+    width: 350,
+    height: '100%',
+    marginTop: 20, // Adjust margin top as needed
+    borderRadius: 10,
+    
+  },
+  treatment: {
+    backgroundColor: 'white',
+    width: 350,
+    height: 200,
+    marginTop: 20, // Adjust margin top as needed
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  buttonModif:{
+    alignItems: 'center',
+    paddingTop: 12,
+    height: 45,
+    width: 200,
+    marginTop: '10%',
+    backgroundColor: '#7368BF',
+    borderRadius: 10,
+    
+  },
+  textButton: {
+    flex: 1,
+    color: '#ffffff',
+    height: 30,
+    fontWeight: '600',
+    fontSize: 16,
+  }
 });
