e532ca91d53a146895a2cf043e73198b7d49d807
Your name <Your email>
2024-03-05 15:55:24 +0100

    jnhj

--- frontend/screens/SignIn.jsx
+++ frontend/screens/SignIn.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image, KeyboardAvoidingView, Platform, View, TextInput} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, Image, KeyboardAvoidingView, Platform, View, TextInput } from 'react-native';
 import KeyboardAwareScrollView from 'react-native-keyboard-aware-scroll-view';
 import { useDispatch } from 'react-redux';
 import { login } from '../reducers/user';
 
 
-export default function SignInScreen({navigation}) {
+export default function SignInScreen({ navigation }) {
 
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
@@ -15,32 +15,32 @@ export default function SignInScreen({navigation}) {
   const handleConnection = () => {
 
     fetch('http://10.9.1.94:3000/users/signin', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: email, password: password }),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, password: password }),
     }).then(response => response.json())
-        .then(data => {
-            if (data.result) {
-              console.log(data)
-                dispatch(login({ prenom: data.user.prenom, nom: data.user.nom, token: data.user.token, idUser: data.user._id, traitements : data.user.traitements }));
-                setEmail('');
-                setPassword('');
-                navigation.navigate('TabNavigator')
-                setSignInError(false)
-            } else {
-              setSignInError(true);
-            }
-        });
+      .then(data => {
+        if (data.result) {
+          console.log(data)
+          dispatch(login({ prenom: data.user.prenom, nom: data.user.nom, token: data.user.token, idUser: data.user._id, traitements: data.user.traitements }));
+          setEmail('');
+          setPassword('');
+          navigation.navigate('TabNavigator')
+          setSignInError(false)
+        } else {
+          setSignInError(true);
+        }
+      });
   };
 
 
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-    <Image style={styles.image} source={require('../assets/TemplateImage.png')}/>
-    <View>
-      <Text style={styles.titre}>Email</Text>
-    <TextInput
+        {/* <Image style={styles.image} source={require('../assets/TemplateImage.png')}/> */}
+        <View>
+          <Text style={styles.titre}>Email</Text>
+          <TextInput
             placeholder="Email"
             autoCapitalize="none"
             keyboardType="email-address"
@@ -50,9 +50,9 @@ export default function SignInScreen({navigation}) {
             style={styles.inputEmail}
             inputMode='email'
           />
-          
-      <Text style={styles.titre}>Mot de passe</Text>      
-    <TextInput
+
+          <Text style={styles.titre}>Mot de passe</Text>
+          <TextInput
             placeholder="Mot de passe"
             autoCapitalize="none"
             secureTextEntry={true}
@@ -61,12 +61,12 @@ export default function SignInScreen({navigation}) {
             style={styles.inputPassword}
           />
 
-  {signInError && <Text style={styles.error}>Utilisateur introuvable ou mot de passe erroné</Text>}
+          {signInError && <Text style={styles.error}>Utilisateur introuvable ou mot de passe erroné</Text>}
 
-    </View>
-      <TouchableOpacity style={styles.buttonSignIn} activeOpacity={0.8}  onPress={() => handleConnection()}> 
-        <Text style={styles.textButton}>Connexion</Text>
-      </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.buttonSignIn} activeOpacity={0.8} onPress={() => handleConnection()}>
+          <Text style={styles.textButton}>Connexion</Text>
+        </TouchableOpacity>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -92,16 +92,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#7368BF',
     borderRadius: 10,
   },
-  titre : {
+  titre: {
     marginBottom: 8,
   },
   inputEmail: {
-   backgroundColor: '#fff',
-   height: 45,
-   width: 286,
-   borderRadius: 10,
-   paddingLeft: 20,
-   marginBottom: 25,
+    backgroundColor: '#fff',
+    height: 45,
+    width: 286,
+    borderRadius: 10,
+    paddingLeft: 20,
+    marginBottom: 25,
   },
   inputPassword: {
     backgroundColor: '#fff',
@@ -110,8 +110,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingLeft: 20,
     marginBottom: 30,
-   },
-   textButton: {
+  },
+  textButton: {
     flex: 1,
     color: '#ffffff',
     height: 30,
