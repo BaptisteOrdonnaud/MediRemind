7170a532b9ccc3ce84544a4a3ae94658799b48f9
Your name <Your email>
2024-03-07 15:17:19 +0100

    debut du fetch et useEffect pour reccuperer les nom et la quantité de medicament

--- frontend/components/MedicamentTraitement.jsx
+++ frontend/components/MedicamentTraitement.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -6,7 +6,22 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-function MedicamentTraitement() {
+function MedicamentTraitement(_id) {
+
+    const [medicaments, setMedicaments] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://10.9.1.94:3000/traitements/${_id}`)
+        .then(response => response.json())
+        .then(drug => {
+            setMedicaments(drug.traitements);
+        })
+        .catch(error => {
+            console.error('erreur lors de la reccuperation des données:', error);
+        });
+    },[]);
+E
+
     return (
         <View style={styles.container}>
             <View style={styles.content}>
@@ -22,6 +37,7 @@ function MedicamentTraitement() {
                 <Text style={styles.heureDePrise}>9:00</Text>
                 <TouchableOpacity style={styles.button} activeOpacity={0.8}>
                     <Text style={styles.textButton}>J'ai pris ce médicament</Text>
+                    <FontAwesome name='check-circle-o' style={styles.icon} />
                 </TouchableOpacity>
             </View>
         </View>
@@ -66,7 +82,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-evenely',
-        padding: '10%',
+        padding: '7%',
     },
     heureDePrise: {
         backgroundColor: 'rgba(166, 154, 252, 0.8)',
@@ -74,19 +90,26 @@ const styles = StyleSheet.create({
         paddingVertical: '1.5%',
         paddingHorizontal:'5%',
         borderRadius: 10,
-        marginRight: '10%',
+        marginRight: '5%',
         marginLeft:'1%',
     },
     button: {
         backgroundColor: '#fff',
         paddingVertical: '1.5%',
         paddingHorizontal: '2%',
-        borderRadius: '1%',
+        borderRadius: 10,
         borderWidth:1,
-        borderColor:'#7368BF'
+        borderColor:'#7368BF',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     textButton: {
         color: '#7368BF',
         fontWeight: 'bold',
     },
+    icon:{
+        color:'#7368BF',
+        paddingLeft:'3%',
+        fontSize:20,
+    }
 });
